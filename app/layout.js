867c1fd418d9927234413ac0122f3f12ae65a6fc
SHEEN MILGER A. OCHAVO <90596472+shin-default123@users.noreply.github.com>
2025-08-15 07:56:00 +0800

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -33,10 +33,6 @@ export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
     <html lang="en">
-      <head>
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6596634799308501"
-     crossorigin="anonymous"></script>
-      </head>
       <body className={inter.className}>
         <Provider>
           <UserLocationContext.Provider value={{userLocation,setUserLocation}}>
